2daa3b3b187601063207b47e3551a45184e611c4
Choyon Ahmed <choyonbd.tk@gmail.com>
2019-02-21 19:15:29 +0600

    Update proxy list.py

--- proxy list.py
+++ proxy list.py
@@ -1,18 +1,23 @@
+# Update: 21.02.2019
+# What's new?
+# Added threading in each requests to speed up the script
+# Added custom number of proxies generation capability
+import threading
 import socket
 import urllib
-socket.setdefaulttimeout(15)
-open('IPList.txt', 'a+').close()
-try:
-    target_source = urllib.urlopen('https://www.proxynova.com/proxy-server-list/').read()
-except:
-    print 'Target server unreachable ...\nHalting ...'
-    exit(0)
-hosts = target_source.split('abbr title="')
-lop = 0
-for i in hosts:
-    lop += 1
-    if lop == 1:
-        continue
+from time import sleep
+socket.setdefaulttimeout(5)
+
+class FuncThread(threading.Thread):
+    def __init__(self, target, *args):
+        self._target = target
+        self._args = args
+        threading.Thread.__init__(self)
+ 
+    def run(self):
+        self._target(*self._args)
+
+def collect(i, target_source,):
     host = i.split('"')[0]
     port = ''
     try:
@@ -29,3 +34,25 @@ for i in hosts:
             print server
     except:
         pass
+
+def execute():
+    open('IPList.txt', 'a+').close()
+    try:
+        target_source = urllib.urlopen('https://www.proxynova.com/proxy-server-list/').read()
+    except:
+        print 'Target server unreachable ...\nHalting ...'
+        exit(0)
+    hosts = target_source.split('abbr title="')
+    lop = 0
+    for i in hosts:
+        lop += 1
+        if lop == 1:
+            continue
+        t1 = FuncThread(collect, i, target_source)
+        t1.start()
+        t1.join()
+
+times = raw_input('How many times to run the collector?(Each time upto 25 proxies):\n')
+for i in range(int(times)):
+    execute()
+    sleep(1)
